90bc142f12e8c48f40ef27fa1415c8a45aa76c5a
Van Quy <vivanquy20804@gmail.com>
2023-12-13 19:23:47 +0700

    Create Model Student

--- model/Student.cpp
+++ model/Student.cpp
@@ -1,1 +1,45 @@
-s
+#include <iostream>
+using namespace std;
+class Student{
+	private:
+		int ID;
+		string Hoten;
+		int Tuoi;
+		string SDT;
+		double GPA;
+	public:
+		//setter
+		void setID(int ID){
+			ID = ID;
+		}
+		void setHoten(string Hoten){
+			Hoten = Hoten;
+		}
+		void setTuoi(int Tuoi){
+			Tuoi = Tuoi;
+		}
+		void setSDT(string SDT){
+			SDT = SDT;
+		}
+		void setGPA(double GPA){
+			GPA = GPA;
+		}
+		//getter
+		int getID(){
+			return ID;
+		}
+		string getHoten(){
+			return Hoten;
+		}
+		int getTuoi(){
+			return Tuoi;
+		}
+		string getSDT(){
+			return SDT;
+		}
+		double getGPA(){
+			return GPA;
+		}
+		
+		
+};
