8fa5338214c5ef04c1294546fc6c4438b13e5207
Van Quy <vivanquy20804@gmail.com>
2023-12-13 20:37:38 +0700

    Relation KhoaHoc and SinhVien

--- model/Models.cpp
+++ model/Models.cpp
@@ -22,77 +22,28 @@ class ChiaseDL{
 		}
 };
 // Tao lop nguoi
-class People: public ChiaseDL{
+class Nguoi: public ChiaseDL{
 	private:
 		int Tuoi;
 		string SDT;
 	public:
 		//setter
 		void setTuoi(int Tuoi){
-			this-> = Tuoi;
+			this->Tuoi = Tuoi;
 		}
 		void setSDT(string SDT){
 			this->SDT = SDT;
 		}
 		//getter
 		int getTuoi(){
-			return this->uoi;
+			return this->Tuoi;
 		}
 		string getSDT(){
 			return SDT;
 		}
 };
-// Tao lop giang vien
-class Teacher : public People{
-	private:
-		double Luong;
-		int StudentID[5];
-	public:
-		//setter
-		void setLuong(double Luong){
-			this->Luong = Luong;
-		}
-		void setStudentID(int StudentID[5]){
-			for(int i=0; i<sizeof(StudentID)/sizeof(StudentID[0]); i++){
-				this->StudentID[i] = StudentID[i];
-			}
-		}
-		//getter
-		double getLuong(){
-			return this->Luong;
-		}
-		int *setStudentID(){
-			return this->StudentID;
-		}
-};
-// Tao lop sinh vien
-class Student: public People{
-	private:
-		double GPA;
-		string n;
-		Teacher tch[5];
-	public:
-		//setter
-		void setGPA(double GPA){
-			this->GPA = GPA;
-		}
-		
-		void setTeacher(Teacher tch[5]){
-			for(int i=0; i<sizeof(tch)/sizeof(tch[0]); i++){
-				this->tch[i] = tch[i];
-			}
-		}
-		//getter
-		double getGPA(){
-			return this->GPA;
-		}
-		Teacher * getTeacher(){
-			return this->tch;
-		}
-		
-};
 //Tao lop khoa hoc
-class KhoaHoc{
+class KhoaHoc: public ChiaseDL{
 	private:
 		double Gio;
 	public:
@@ -101,3 +52,61 @@ class KhoaHoc{
 			this->Gio = Gio
 		}
 };
+// Tao lop giang vien
+class GiangVien : public Nguoi{
+	private:
+		double Luong;
+		int IDSinhVien[5];
+	public:
+		//setter
+		void setLuong(double Luong){
+			this->Luong = Luong;
+		}
+		void setIDSinhVien(int IDSinhVien[5]){
+			for(int i=0; i<sizeof(IDSinhVien)/sizeof(IDSinhVien[0]); i++){
+				this->IDSinhVien[i] = IDSinhVien[i];
+			}
+		}
+		//getter
+		double getLuong(){
+			return this->Luong;
+		}
+		int *setIDSinhVien(){
+			return this->IDSinhVien;
+		}
+};
+// Tao lop sinh vien
+class SinhVien: public Nguoi{
+	private:
+		double GPA;
+		string n;
+		GiangVien GV[5];
+		KhoaHoc KH[5];
+	public:
+		//setter
+		void setGPA(double GPA){
+			this->GPA = GPA;
+		}
+		
+		void setGV(GiangVien GV[5]){
+			for(int i=0; i<sizeof(GV)/sizeof(GV[0]); i++){
+				this->GV[i] = GV[i];
+			}
+		}
+		void setKH(KhoaHoc KH[5]){
+			for(int i=0; i<sizeof(KH)/sizeof(KH[0]); i++){
+				this->KH[i] = KH[i];
+			}
+		}
+		//getter
+		double getGPA(){
+			return this->GPA;
+		}
+		GiangVien * getGV(){
+			return this->GV;
+		}
+		KhoaHoc * getKH(){
+			return this->KH;
+		}
+};
+
