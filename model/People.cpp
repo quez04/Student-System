2e7b4a247b9c64b286f3a67aa0f7f7cc50c18769
Van Quy <vivanquy20804@gmail.com>
2023-12-13 19:43:43 +0700

    Create Model People

--- model/People.cpp
+++ model/People.cpp
@@ -1,1 +1,36 @@
 #include <iostream>
+using namespace std;
+class People{
+	private:
+		int ID;
+		string Hoten;
+		int Tuoi;
+		string SDT;
+	public:
+		//setter
+		void setID(int ID){
+			ID = ID;
+		}
+		void setHoten(string Hoten){
+			Hoten = Hoten;
+		}
+		void setTuoi(int Tuoi){
+			Tuoi = Tuoi;
+		}
+		void setSDT(string SDT){
+			SDT = SDT;
+		}
+		//getter
+		int getID(){
+			return ID;
+		}
+		string getHoten(){
+			return Hoten;
+		}
+		int getTuoi(){
+			return Tuoi;
+		}
+		string getSDT(){
+			return SDT;
+		}
+};
--- model/Student.cpp
+++ model/Student.cpp
@@ -1,41 +1,13 @@
-
-class Student{
+#include "People.cpp"
+class Student: public People{
 	private:
-		int ID;
-		string Hoten;
-		int Tuoi;
-		string SDT;
 		double GPA;
 	public:
 		//setter
-		void setID(int ID){
-			ID = ID;
-		}
-		void setHoten(string Hoten){
-			Hoten = Hoten;
-		}
-		void setTuoi(int Tuoi){
-			Tuoi = Tuoi;
-		}
-		void setSDT(string SDT){
-			SDT = SDT;
-		}
 		void setGPA(double GPA){
 			GPA = GPA;
 		}
 		//getter
-		int getID(){
-			return ID;
-		}
-		string getHoten(){
-			return Hoten;
-		}
-		int getTuoi(){
-			return Tuoi;
-		}
-		string getSDT(){
-			return SDT;
-		}
 		double getGPA(){
 			return GPA;
 		}
