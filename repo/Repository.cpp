b869075232d02a74e6835c149d4d55c2c28e4a06
Van Quy <vivanquy20804@gmail.com>
2023-12-15 09:21:54 +0700

    Repo Student

---
+++ repo/Repository.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/////////////////student////////////
+class khoSV{
+};
+class khoSVImpl: public khoSV{
+};
+
+/////////////////course////////////
+class khoKH{
+};
+class khoKHImpl: public khoKH{
+};
+
+/////////////////teacher////////////
+class khoGV{
+};
+class khoGVImpl: public khoGV{
+};
